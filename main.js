ccf426383623987a86278e1002ddf14e3d23de9c
Aikidooo <kekskonrixnixlp@gmail.com>
2022-12-15 13:03:12 +0100

    Added basic logic

---
+++ main.js
@@ -0,0 +1,70 @@
+const config = require("./config.json");
+
+const token = config.token;
+
+
+const Discord = require("discord.js");
+const Client = new Discord.Client({
+    intents: [Discord.IntentsBitField.Flags.GuildMessages, Discord.IntentsBitField.Flags.Guilds],
+});
+const fs = require("fs");
+let channel;
+
+
+
+console.log("Starting...");
+Client.on("ready", async () => {
+    channel = await Client.channels.fetch(config.channel);
+    
+    console.log("Bot is ready");
+    channel.send("--------------------Bot is ready--------------------");
+    
+    let first = true;
+    let lastLines = [];
+
+    setInterval(async () => {
+        if(!fs.existsSync("screenlog.0")) {
+            console.log("Couldn't find a logfile, please add/rename one to screenlog.0");
+            await Sleep(20000);
+            return;
+        }
+        let logs = fs.readFileSync("screenlog.0");
+        let lines = logs.toString().split("\n");
+        if(lines.length < 2) return;
+        if(first) {
+            lines.forEach((line) => {
+                if(!line) return;
+                channel.send(line)
+            });
+            lastLines = lines;
+            first = false;
+            return;
+        }
+        let oldLastLine = lastLines.length;
+        let newLastLine = lines.length;
+        if(oldLastLine != newLastLine) {
+            let linesToPrint = lines.slice(oldLastLine);
+            linesToPrint.forEach((line) => channel.send(line));
+        }
+        lastLines = lines;
+
+    }, 500); //500ms update interval
+
+    process.on("exit", () => {
+        console.log("Stopping the bot...");
+        if(channel) {
+            channel.send("--------------------Bot stopped--------------------");
+            console.log("Bot stopped.")
+        } else {
+            console.log("Bot wasn't yet connected to Channel when stopped.")
+        }
+    });
+});
+
+
+
+function Sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+Client.login(token);
